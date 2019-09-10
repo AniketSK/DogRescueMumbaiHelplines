609b85cdf6cbacc29e3aa2dc990a071f1a09a34a
Aniket Kadam <anikadamg@gmail.com>
2019-09-10 23:14:30 +0530

    Ensuring isLocation crash if actually used in production instead of temp values.

--- app/src/main/java/com/aniketkadam/dogrescuemumbai/LocationProvider.kt
+++ app/src/main/java/com/aniketkadam/dogrescuemumbai/LocationProvider.kt
@@ -4,7 +4,7 @@ import com.aniketkadam.dogrescuemumbai.data.Location
 
 class LocationProvider {
     fun isLocationGranted(): Boolean {
-        return false
+        TODO()
     }
 
     fun getCurrentLocation(): Location {
