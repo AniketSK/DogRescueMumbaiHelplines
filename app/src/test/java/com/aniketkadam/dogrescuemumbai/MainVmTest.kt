bbc0530d5ee796f5087a3b4211bd28d8f23ba969
Aniket Kadam <anikadamg@gmail.com>
2019-09-10 23:03:04 +0530

    Created a test for the VM to ask for location permission

---
+++ app/src/test/java/com/aniketkadam/dogrescuemumbai/MainVmTest.kt
@@ -0,0 +1,29 @@
+package com.aniketkadam.dogrescuemumbai
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import io.mockk.every
+import io.mockk.mockk
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.equalTo
+import org.junit.Rule
+import org.junit.Test
+
+class MainVmTest {
+
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Test
+    fun `when the app starts, location is requested if it is not already granted`() {
+        val locationProvider = mockk<LocationProvider> {
+            every { isLocationGranted() } returns false
+        }
+
+        val mainVm = MainVm(locationProvider)
+
+        assertThat(
+            mainVm.viewState.getOrAwaitValue(),
+            equalTo<ViewState>(ViewState.RequestLocationPermission)
+        )
+    }
+}
